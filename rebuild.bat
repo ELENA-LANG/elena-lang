ed31e1fe2e689402d32d59b8af2cd8dbc9ba04c5
arakov <arakov@yandex.ru>
2015-11-22 18:10:13 +0100

    (re)implementing LIB30

--- rebuild.bat
+++ rebuild.bat
@@ -15,6 +15,6 @@ bin\asm2binx src30\asm\x32\core_rt.asm bin\x32
 bin\asm2binx src30\asm\x32\core_win.asm bin\x32
 bin\elc -csrc30\system\system.prj
 bin\elc -csrc30\extensions\extensions.prj
-rem bin\elc -csrc30\net\net.prj
+bin\elc -csrc30\net\net.prj
 bin\elc -csrc30\forms\forms.prj
-rem bin\elc -csrc30\sqlite\sqlite.prj
+bin\elc -csrc30\sqlite\sqlite.prj
