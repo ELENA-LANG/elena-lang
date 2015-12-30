259bd3f2cf59c36664d2d95bce8311bfdcd2e00d
arakov <arakov@yandex.ru>
2015-12-30 12:33:05 +0100

    fixing compare a list of string sample

--- vs_release.bat
+++ vs_release.bat
@@ -659,8 +659,12 @@ copy %1\examples\rosetta\dynamic_var\*.l examples\rosetta\dynamic_var
 copy %1\examples\rosetta\dynamic_var\*.prj examples\rosetta\dynamic_var
 
 md examples\rosetta\firstclass
-copy %1\examples\rosetta\firstclass \*.l examples\rosetta\firstclass
+copy %1\examples\rosetta\firstclass\*.l examples\rosetta\firstclass
 copy %1\examples\rosetta\firstclass\*.prj examples\rosetta\firstclass
+
+md examples\rosetta\loop_multiple_arrays
+copy %1\examples\rosetta\loop_multiple_arrays\*.l examples\rosetta\loop_multiple_arrays
+copy %1\examples\rosetta\loop_multiple_arrays\*.prj examples\rosetta\loop_multiple_arrays
 
 bin\elc -csrc30\system\system.prj
 bin\elc -csrc30\extensions\extensions.prj
