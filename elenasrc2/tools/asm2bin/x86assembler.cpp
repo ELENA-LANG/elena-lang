082303eff991ddedf47b144a9855598039141e51
Alexandre <alebencz@gmail.com>
2016-01-15 17:22:15 -0200

    Fix movups instruction

--- elenasrc2/tools/asm2bin/x86assembler.cpp
+++ elenasrc2/tools/asm2bin/x86assembler.cpp
@@ -2031,9 +2031,14 @@ void x86Assembler :: compileMOVUPS(TokenInfo& token, ProcedureInfo& info, Memory
       code->writeByte(0x10);
       x86Helper::writeModRM(code, sour, dest);
    }
-   else if ((test(sour.type, x86Helper::otX128) || test(sour.type, x86Helper::otM32)) && test(dest.type, x86Helper::otX128)) {
+   else if (test(sour.type, x86Helper::otX128) && test(dest.type, x86Helper::otX128)) {
       code->writeByte(0x11);
       x86Helper::writeModRM(code, sour, dest);
+   }
+   else if (test(sour.type, x86Helper::otM32) && test(dest.type, x86Helper::otX128)) {
+	   code->writeByte(0x11);
+	   x86Helper::writeModRM(code, Operand(x86Helper::otR32 + 3), sour);
+	   x86Helper::writeModRM(code, sour, dest);
    }
    else token.raiseErr("Invalid command (%d)");
 }
