5bf1ba7130ffed8ab169191215a93da094bdd6ac
Alexandre <alebencz@gmail.com>
2016-01-21 09:36:43 -0200

    Deleted mathapi.

--- rebuild.bat
+++ rebuild.bat
@@ -6,11 +6,9 @@ move dat\og\rules.dat bin
 
 bin\asm2binx src30\asm\core_routines.esm lib30\system
 bin\asm2binx src30\asm\ext_routines.esm lib30\system
-bin\asm2binx src30\asm\math_routines.esm lib30\system
 bin\asm2binx src30\asm\x32\core.asm bin\x32
 bin\asm2binx src30\asm\x32\corex.asm bin\x32
 bin\asm2binx src30\asm\x32\coreapi.asm bin\x32
-bin\asm2binx src30\asm\x32\mathapi.asm bin\x32
 bin\asm2binx src30\asm\x32\core_vm.asm bin\x32
 bin\asm2binx src30\asm\x32\core_rt.asm bin\x32
 bin\asm2binx src30\asm\x32\core_win.asm bin\x32
