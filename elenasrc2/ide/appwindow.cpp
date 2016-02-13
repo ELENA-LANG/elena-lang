033edce28c725ffc43f8474cdebf92476592a9ec
arakov <arakov@yandex.ru>
2016-02-13 23:43:02 +0100

    #4: fixing code

--- elenasrc2/ide/appwindow.cpp
+++ elenasrc2/ide/appwindow.cpp
@@ -59,7 +59,7 @@ const text_c* lexDFA[] =
 
 text_c makeStep(text_c ch, text_c state)
 {
-   return (unsigned text_c)ch < 128 ? lexDFA[state - lexStart][ch] : lexDFA[state - lexStart][127];
+   return (unsigned)ch < 128 ? lexDFA[state - lexStart][ch] : lexDFA[state - lexStart][127];
 }
 
 size_t defineStyle(text_c state, size_t style)
