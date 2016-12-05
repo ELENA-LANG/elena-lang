56e4c416fd63765d0a2c79ca5047d297ef196a79
arakov <arakov@yandex.ru>
2016-12-05 14:55:34 +0100

    adding missing files

---
+++ elenasrc2/elenasm/treeparser.cpp
@@ -0,0 +1,84 @@
+//---------------------------------------------------------------------------
+//		E L E N A   P r o j e c t:  ELENA VM Script Engine
+//
+//                                              (C)2011-2016, by Alexei Rakov
+//---------------------------------------------------------------------------
+
+#include "elena.h"
+// --------------------------------------------------------------------------
+#include "treeparser.h"
+#include "bytecode.h"
+
+using namespace _ELENA_;
+using namespace _ELENA_TOOL_;
+
+// --- TreeScriptParser ---
+
+TreeScriptParser :: TreeScriptParser()
+{
+   _tokens.add("root", lxRoot);
+   _tokens.add("class", lxClass);
+   _tokens.add("method", lxClassMethod);
+   _tokens.add("message", lxMessage);
+   _tokens.add("code", lxCode);
+   _tokens.add("expression", lxExpression);
+   _tokens.add("symbol", lxReference);
+   _tokens.add("literal", lxLiteral);
+}
+
+void TreeScriptParser :: parseScope(_ScriptReader& reader, ScriptBookmark& bm, SyntaxWriter& writer)
+{
+   bm = reader.read();
+   while (!reader.Eof() && !reader.compare(")")) {
+      parseStatement(reader, bm, writer);
+
+      bm = reader.read();
+   }
+
+}
+
+void TreeScriptParser :: parseStatement(_ScriptReader& reader, ScriptBookmark& bm, SyntaxWriter& writer)
+{
+   int type = _tokens.get(reader.lookup(bm));
+   if (type != 0) {
+      writer.newNode((LexicalType)type);
+
+      bm = reader.read();
+      if (reader.compare("(")) {
+         parseScope(reader, bm, writer);
+      }
+      else if (reader.compare("=")) {
+         bm = reader.read();
+
+         writer.appendNode(lxTerminal, reader.lookup(bm));
+      }
+      else throw EParseError(bm.column, bm.row);
+
+      writer.closeNode();
+   }
+   else throw EParseError(bm.column, bm.row);
+}
+
+void TreeScriptParser :: parse(_ScriptReader& reader, MemoryDump* output)
+{
+   MemoryWriter writer(output);
+   writer.writeDWord(0); // HOTFIX : end of tape
+   int sizePos = writer.Position();
+   writer.writeDWord(0);
+
+   SyntaxTree tree;
+   SyntaxWriter treeWriter(tree);
+
+   ScriptBookmark bm = reader.read();
+   while (!reader.Eof()) {      
+      parseStatement(reader, bm, treeWriter);
+
+      bm = reader.read();
+   }   
+
+   if (tree.save(output)) {
+      writer.seek(sizePos);
+      writer.writeDWord(output->Length() - sizePos);
+   }
+   else output->clear();
+}
---
+++ elenasrc2/elenasm/treeparser.h
@@ -0,0 +1,38 @@
+//---------------------------------------------------------------------------
+//		E L E N A   P r o j e c t:  ELENA VM Script Engine
+//
+//                                             (C)2011-2016, by Alexei Rakov
+//---------------------------------------------------------------------------
+
+#ifndef treearserH
+#define treearserH 1
+
+#include "scriptengine.h"
+#include "syntaxtree.h"
+
+namespace _ELENA_
+{
+
+// --- TreeScriptParser ---
+
+class TreeScriptParser : public _Parser
+{
+   Map<ident_t, int> _tokens;
+
+   void parseScope(_ScriptReader& reader, ScriptBookmark& bm, SyntaxWriter& writer);
+   void parseStatement(_ScriptReader& reader, ScriptBookmark& bm, SyntaxWriter& writer);
+
+public:
+   virtual bool parseGrammarRule(_ScriptReader& reader)
+   {
+      return false;
+   }
+
+   virtual void parse(_ScriptReader& reader, MemoryDump* output);
+
+   TreeScriptParser();
+};
+
+} // _ELENA_
+
+#endif // treearserH
