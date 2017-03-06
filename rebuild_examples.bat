421873626b964c33048249cc4c73f4cbd4906d5e
arakov <arakov@yandex.ru>
2017-03-06 15:58:40 +0100

    working on samples

--- rebuild_examples.bat
+++ rebuild_examples.bat
@@ -10,10 +10,10 @@ bin\elc examples\console\binary\binary.prj
 bin\elc examples\console\replace\replace.prj
 bin\elc examples\console\goods\goods.prj
 bin\elc examples\console\datetime\dtcalculator.prj
-rem bin\elc examples\console\random\random.prj
-rem bin\elc examples\files\textdb\textdb.prj
-rem bin\elc examples\console\matrix\matrix.prj
-rem bin\elc examples\console\trans\tanslit.prj
+bin\elc examples\console\random\random.prj
+bin\elc examples\files\textdb\textdb.prj
+bin\elc examples\console\matrix\matrix.prj
+bin\elc examples\console\trans\tanslit.prj
 rem bin\elc examples\script\interpreter\interpreter.prj
 rem bin\elc examples\script\calculator\calc.xprj
 rem bin\elc examples\script\calculator\calc.prj
@@ -22,6 +22,6 @@ rem bin\elc examples\gui\graphs\graphs.prj
 rem bin\elc examples\gui\c_a_g\c_area_gui.prj
 rem bin\elc examples\gui\notepad\notepad.prj
 rem bin\elc examples\db\sqlite\sqlite_test.prj
-rem bin\elc examples\console\pi2\pi2.prj
+bin\elc examples\console\pi2\pi2.prj
 rem bin\elc examples\net\chat\chatserver.prj
 rem bin\elc examples\net\chat\chat.prj
