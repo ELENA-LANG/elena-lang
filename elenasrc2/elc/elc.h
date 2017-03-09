10561107a4af150523089f8e8b9a9bf6a08e9b36
arakov <arakov@yandex.ru>
2017-03-09 15:41:25 +0100

    working on event template

--- elenasrc2/elc/elc.h
+++ elenasrc2/elc/elc.h
@@ -14,7 +14,7 @@
 #include "jitcompiler.h"
 
 // --- ELC common constants ---
-#define ELC_REVISION_NUMBER             0x0051
+#define ELC_REVISION_NUMBER             0x0052
 
 // --- ELC default file names ---
 #ifdef _WIN32
