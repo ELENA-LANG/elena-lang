471477b49e1eccf8c8cf8c93ead45de290fba9e7
arakov <arakov@yandex.ru>
2017-04-18 17:31:23 +0200

    fixing : template generated class routine

--- elenasrc2/elc/elc.h
+++ elenasrc2/elc/elc.h
@@ -14,7 +14,7 @@
 #include "jitcompiler.h"
 
 // --- ELC common constants ---
-#define ELC_REVISION_NUMBER             0x0082
+#define ELC_REVISION_NUMBER             0x0083
 
 // --- ELC default file names ---
 #ifdef _WIN32
