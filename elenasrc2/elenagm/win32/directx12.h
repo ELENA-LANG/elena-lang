19df6f3ee4924092c1a808db1508d9971b426b56
arakov <arakov@yandex.ru>
2017-06-22 17:26:46 +0200

    working on DirectX wrapper

---
+++ elenasrc2/elenagm/win32/directx12.h
@@ -0,0 +1,78 @@
+//---------------------------------------------------------------------------
+//		E L E N A   P r o j e c t:  ELENA Graphic Engine
+//
+//                                              (C)2017, by Alexei Rakov
+//---------------------------------------------------------------------------
+
+#ifndef elenagm_d12H
+#define elenagm_d12H 1
+
+#include "win32_common.h"
+
+using namespace DirectX;
+
+class D12Platform
+{
+	static const UINT FrameCount = 3;
+
+	struct Vertex
+	{
+		XMFLOAT3 position;
+		XMFLOAT4 color;
+	};
+
+	int		_width;
+	int		_height;
+	float	_aspectRatio;
+
+	// Pipeline objects.
+	CD3DX12_VIEWPORT					_viewport;
+	ComPtr<ID3D12Device>				_d3d12Device;
+	ComPtr<ID3D11On12Device>			_d3d11On12Device;
+	ComPtr<ID3D11DeviceContext>			_d3d11DeviceContext;
+	ComPtr<ID2D1Factory3>				_d2dFactory;
+	ComPtr<ID2D1Device2>				_d2dDevice;
+	ComPtr<ID2D1DeviceContext2>			_d2dDeviceContext;
+	ComPtr<IDWriteFactory>				_dWriteFactory;
+	ComPtr<ID3D12CommandQueue>			_commandQueue;
+	ComPtr<IDXGISwapChain3>				_swapChain;
+	ComPtr<ID3D12DescriptorHeap>		_rtvHeap;
+	ComPtr<ID3D12Resource>				_renderTargets[FrameCount];
+	ComPtr<ID3D11Resource>				_wrappedBackBuffers[FrameCount];
+	ComPtr<ID2D1Bitmap1>				_d2dRenderTargets[FrameCount];
+	ComPtr<ID3D12CommandAllocator>		_commandAllocators[FrameCount];
+	ComPtr<ID3D12PipelineState>			_pipelineState;
+	ComPtr<ID3D12GraphicsCommandList>	_commandList;
+	ComPtr<ID3D12RootSignature>			_rootSignature;
+
+	// App resources.
+	UINT								_rtvDescriptorSize;
+	ComPtr<ID2D1SolidColorBrush>		_textBrush;
+	ComPtr<IDWriteTextFormat>			_textFormat;
+	ComPtr<ID3D12Resource>				_vertexBuffer;
+	D3D12_VERTEX_BUFFER_VIEW			_vertexBufferView;
+
+	// Synchronization objects.
+	UINT								_frameIndex;
+	HANDLE								_fenceEvent;
+	UINT64								_fenceValues[FrameCount];
+	ComPtr<ID3D12Fence>					_fence;
+
+	void MoveToNextFrame();
+
+	void PopulateCommandList();
+	void RenderUI();
+
+	void WaitForGpu();
+
+public:
+	D12Platform(int width, int height);
+
+	void Init(HWND hWnd, int sampleCount);
+	void LoadAssets();
+
+	void OnRender();
+	void OnDestroy();
+};
+
+#endif // elenagm_d12H
--- elenasrc2/elenagm/win32/dllmain.cpp
+++ elenasrc2/elenagm/win32/dllmain.cpp
@@ -1,88 +1,56 @@
 #include "elena.h"
 // --------------------------------------------------------------------------
+#include "win32_common.h"
+#include "directx12.h"
 
-#include <windows.h>
-#include <wrl.h>
-
-#include <d3d12.h>
-#include <dxgi1_4.h>
-
-using Microsoft::WRL::ComPtr;
+//
+//using Microsoft::WRL::ComPtr;
 
 #define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
 
-// global declarations
-IDXGISwapChain1*	swapchain;             // the pointer to the swap chain interface
-ID3D12Device*		devD12;                     // the pointer to our Direct3D device interface
-ID3D12CommandQueue*	commandQueue;
-//ID3D11Device* dev;
-//ID3D11DeviceContext *devcon;           // the pointer to our Direct3D device context
+D12Platform* d12Platform = NULL;
+
+//// global declarations
+//IDXGISwapChain1*	swapchain;             // the pointer to the swap chain interface
+//ID3D12Device*		devD12;                     // the pointer to our Direct3D device interface
+//ID3D12CommandQueue*	commandQueue;
+////ID3D11Device* dev;
+////ID3D11DeviceContext *devcon;           // the pointer to our Direct3D device context
 
 // === dll entries ===
 
 /// sets up and initializes Direct3D
-EXTERN_DLL_EXPORT void InitD3D(HWND hWnd)
+EXTERN_DLL_EXPORT int InitD3D(HWND hWnd)
 {
-	RECT rect;
-	GetWindowRect(hWnd, &rect);
+	try
+	{
+		RECT rect;
+		GetWindowRect(hWnd, &rect);
 
-	UINT dxgiFactoryFlags = 0;
+		d12Platform = new D12Platform(rect.right - rect.left, rect.bottom - rect.top);
 
-	ComPtr<IDXGIFactory4> factory;
-	CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory));
+		d12Platform->Init(hWnd, 1);
+		d12Platform->LoadAssets();
 
-	ComPtr<IDXGIAdapter> warpAdapter;
-	factory->EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter));
+		return -1;
+	}
+	catch (...)
+	{
+		return 0;
+	}
+}
 
-	D3D12CreateDevice(
-		warpAdapter.Get(),
-		D3D_FEATURE_LEVEL_11_0,
-		IID_PPV_ARGS(&devD12)
-	);
-
-	// Describe and create the command queue.
-	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
-	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
-
-	devD12->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&commandQueue));
-	//NAME_D3D12_OBJECT(commandQueue);
-
-	// create a struct to hold information about the swap chain
-	DXGI_SWAP_CHAIN_DESC1 scd;
-
-	// clear out the struct for use
-	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC1));
-
-	// fill the swap chain description struct
-	scd.BufferCount = 1;                                    // one back buffer
-	scd.Width = rect.right - rect.left;
-	scd.Height = rect.bottom - rect.top;
-	scd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;				// use 32-bit color
-	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
-	scd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-	scd.SampleDesc.Count = 4;                               // how many multisamples
-	//scd.OutputWindow = hWnd;                                // the window to be used
-	//scd.Windowed = TRUE;                                    // windowed/full-screen mode
-
-	factory->CreateSwapChainForHwnd(
-		commandQueue,		// Swap chain needs the queue so that it can force a flush on it.
-		hWnd,
-		&scd,
-		nullptr,
-		nullptr,
-		&swapchain
-	);
+EXTERN_DLL_EXPORT void Render3D(HWND hWnd)
+{
+	d12Platform->OnRender();
 }
 
 /// closes Direct3D and releases memory
-EXTERN_DLL_EXPORT void CleanD3D()
+EXTERN_DLL_EXPORT void CleanD3D(HWND hWnd)
 {
-	// close and release all existing COM objects
-	swapchain->Release();
-	devD12->Release();
-	commandQueue->Release();
-	//devcon->Release();
+	d12Platform->OnDestroy();
+
+	delete d12Platform;
 }
 
 // --- dllmain ---
