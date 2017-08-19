f3f1add46e00ef606e9c562901664340bfdfc6ac
arakov <arakov@yandex.ru>
2017-08-19 14:18:35 +0200

    fixing build

--- rebuild_examples.bat
+++ rebuild_examples.bat
@@ -1,8 +1,8 @@
 bin\elc examples\console\helloworld\u_helloworld.prj
 bin\elc examples\console\helloworld\helloworld.prj
-rem bin\elc examples\console\sum\intsum.prj
-rem bin\elc examples\console\sum\realsum.prj
-rem bin\elc examples\console\words\words.prj
+bin\elc examples\console\sum\intsum.prj
+bin\elc examples\console\sum\realsum.prj
+bin\elc examples\console\words\words.prj
 rem bin\elc examples\console\pi\pi.prj
 rem bin\elc examples\console\bsort\bsort.prj
 rem bin\elc examples\files\textfile\textfile.prj
