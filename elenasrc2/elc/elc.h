06c76c7449c535e2d0a1aca76565add4555e6be1
arakov <arakov@yandex.ru>
2017-12-04 09:48:24 +0100

    fixing template in template routine

--- elenasrc2/elc/elc.h
+++ elenasrc2/elc/elc.h
@@ -14,7 +14,7 @@
 #include "jitcompiler.h"
 
 // --- ELC common constants ---
-#define ELC_REVISION_NUMBER             0x0033
+#define ELC_REVISION_NUMBER             0x0034
 // --- ELC default file names ---
 #ifdef _WIN32
 
