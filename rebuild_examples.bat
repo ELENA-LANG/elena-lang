d3552c02299d236f99ea36f5a4256db485a4ccc4
arakov <arakov@yandex.ru>
2018-05-07 18:48:47 +0200

    migrating samples to LIB34

--- rebuild_examples.bat
+++ rebuild_examples.bat
@@ -6,14 +6,15 @@ bin\elc examples\console\words\words.prj
 bin\elc examples\console\pi\pi.prj
 bin\elc examples\console\bsort\bsort.prj
 bin\elc examples\files\textfile\textfile.prj
-rem bin\elc examples\console\binary\binary.prj
-rem bin\elc examples\console\replace\replace.prj
-rem bin\elc examples\console\goods\goods.prj
+bin\elc examples\console\binary\binary.prj
+bin\elc examples\console\replace\replace.prj
+bin\elc examples\console\goods\goods.prj
 rem bin\elc examples\console\datetime\dtcalculator.prj
 rem bin\elc examples\console\random\random.prj
 rem bin\elc examples\files\textdb\textdb.prj
 rem bin\elc examples\console\trans\tanslit.prj
 rem bin\elc examples\console\matrix\matrix.prj
+rem bin\elc examples\console\pi2\pi2.prj
 rem bin\elc examples\script\interpreter\interpreter.prj
 rem bin\elc examples\script\calculator\calc.xprj
 rem bin\elc examples\script\calculator\calc.prj
@@ -24,7 +25,6 @@ rem bin\elc examples\gui\graphs\graphs.prj
 rem bin\elc examples\gui\c_a_g\c_area_gui.prj
 rem bin\elc examples\gui\notepad\notepad.prj
 rem bin\elc examples\db\sqlite\sqlite_test.prj
-rem bin\elc examples\console\pi2\pi2.prj
 rem bin\elc examples\net\chat\chatserver.prj
 rem bin\elc examples\net\chat\chat.prj
 rem bin\elc examples\gui\helloworld\xforms_hellowindow.prj
