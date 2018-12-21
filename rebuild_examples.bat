3b6929ce21146a5be58454fc108b737a6d32d42a
arakov <arakov@yandex.ru>
2018-12-21 11:07:33 +0100

    some house keeping

--- rebuild_examples.bat
+++ rebuild_examples.bat
@@ -1,5 +1,5 @@
-bin\elc examples\console\helloworld\u_helloworld.prj
-rem bin\elc examples\console\helloworld\helloworld.prj
+bin\elc examples\console\helloworld\helloworld.prj
+rem bin\elc examples\console\helloworld\vm_helloworld.prj
 rem bin\elc examples\console\sum\intsum.prj
 rem bin\elc examples\console\sum\realsum.prj
 rem bin\elc examples\console\words\words.prj
