7483c91cbd028748270fc131576ead3bf4a5be75
arakov <arakov@yandex.ru>
2018-12-25 18:49:24 +0100

    migrating words sample

--- rebuild_examples.bat
+++ rebuild_examples.bat
@@ -2,7 +2,7 @@ bin\elc examples\console\helloworld\helloworld.prj
 rem bin\elc examples\console\helloworld\vm_helloworld.prj
 rem bin\elc examples\console\sum\intsum.prj
 rem bin\elc examples\console\sum\realsum.prj
-rem bin\elc examples\console\words\words.prj
+bin\elc examples\console\words\words.prj
 rem bin\elc examples\console\pi\pi.prj
 rem bin\elc examples\console\bsort\bsort.prj
 rem bin\elc examples\files\textfile\textfile.prj
