15ed2e0bd3c0a3436314d13f1601053bd4ed51aa
arakov <arakov@yandex.ru>
2018-12-29 16:30:39 +0100

    ELENA 4.0 : (re)implementing realsum sample

--- rebuild_examples.bat
+++ rebuild_examples.bat
@@ -1,7 +1,7 @@
 bin\elc examples\console\helloworld\helloworld.prj
 rem bin\elc examples\console\helloworld\vm_helloworld.prj
 bin\elc examples\console\sum\intsum.prj
-rem bin\elc examples\console\sum\realsum.prj
+bin\elc examples\console\sum\realsum.prj
 bin\elc examples\console\words\words.prj
 rem bin\elc examples\console\pi\pi.prj
 rem bin\elc examples\console\bsort\bsort.prj
