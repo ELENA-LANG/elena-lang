ec353e878d4d33779d90689809cb27c01f1fc175
Aleksey Rakov <arakov@yandex.ru>
2020-02-05 12:06:25 +0100

    working on #495

--- elenasrc2/elc/elc.h
+++ elenasrc2/elc/elc.h
@@ -18,7 +18,7 @@
 #include "errors.h"
 
 // --- ELC common constants ---
-#define ELC_REVISION_NUMBER         0x00CE
+#define ELC_REVISION_NUMBER         0x00CF
 
 // --- ELC default file names ---
 #ifdef _WIN32
