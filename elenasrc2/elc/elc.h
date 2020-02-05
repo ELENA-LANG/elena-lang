a03e4e0cc32f014e3eaab87fba2f33123c887712
Aleksey Rakov <arakov@yandex.ru>
2020-02-05 12:00:49 +0100

    working on class constants : accumulating constants

--- elenasrc2/elc/elc.h
+++ elenasrc2/elc/elc.h
@@ -18,7 +18,7 @@
 #include "errors.h"
 
 // --- ELC common constants ---
-#define ELC_REVISION_NUMBER         0x00CD
+#define ELC_REVISION_NUMBER         0x00CE
 
 // --- ELC default file names ---
 #ifdef _WIN32
