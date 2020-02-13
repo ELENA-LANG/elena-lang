410eeab6c5b4e2f307cc94cfc1cc3144a8c1006f
Aleksey Rakov <arakov@yandex.ru>
2020-02-13 16:08:39 +0100

    working on rosetta code samples : 24

--- elenasrc2/elc/elc.h
+++ elenasrc2/elc/elc.h
@@ -18,7 +18,7 @@
 #include "errors.h"
 
 // --- ELC common constants ---
-#define ELC_REVISION_NUMBER         0x00EA
+#define ELC_REVISION_NUMBER         0x00EB
 
 // --- ELC default file names ---
 #ifdef _WIN32
