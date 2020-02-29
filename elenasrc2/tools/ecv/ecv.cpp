3979d3b6e2a27ee8859a952087828eac2d702ed4
Aleksey Rakov <arakov@yandex.ru>
2020-02-29 22:33:55 +0100

    working with rosetta code samples

--- elenasrc2/tools/ecv/ecv.cpp
+++ elenasrc2/tools/ecv/ecv.cpp
@@ -28,7 +28,7 @@
 #define ROOTPATH_OPTION "libpath"
 
 #define MAX_LINE           256
-#define REVISION_VERSION   62
+#define REVISION_VERSION   63
 
 using namespace _ELENA_;
 
@@ -644,7 +644,7 @@ bool printCommand(_Module* module, MemoryReader& codeReader, int indent, List<in
       case bcQuitN:
       case bcDec:
       case bcInc:
-         //      case bcECopy:
+      case bcMul:
       case bcAnd:
       case bcOr:
 //      case bcInit:
