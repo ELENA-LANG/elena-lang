41bf58dfd1e06f46d4a7c7e6842d6875dfdb89b3
Aleksey Rakov <arakov@yandex.ru>
2020-07-10 21:46:53 +0200

    migrating script engine to linux

--- elenasrc2/elenasm/linux32/main.cpp
+++ elenasrc2/elenasm/linux32/main.cpp
@@ -8,231 +8,87 @@
 // -------------------------------------------------------------------
 #include "elenasm.h"
 #include "session.h"
-//#include <unistd.h>
+#include <unistd.h>
 
-//#define ROOT_PATH          "/usr/lib/elena"
-//#define CONFIG_PATH        "/etc/elena/elenart.config"
-//#define IMAGE_BASE         0x08048000
+#define ROOT_PATH        "/etc/elena"
 
 using namespace _ELENA_;
 
-static Session* session = NULL;
+static Session* session = nullptr;
 
-void getSelfPath(Path& rootPath)
+void newSession()
 {
-   char buff[PATH_MAX];
-   size_t len = ::readlink("/proc/self/exe", buff, sizeof(buff) - 1);
-   if (len != -1) {
-      buff[len] = 0;
-      rootPath.copy(buff);
+   session = new Session(ROOT_PATH);
+}
+
+// === dll entries ===
+
+void* InterpretScript(ident_t script)
+{
+   if (!session) {
+      newSession();
    }
-   /* handle error condition */
+
+   return session->translate(0, script);
 }
 
-//inline void printInfo(int n)
-//{
-//   printf("%x\n", n);
-//   fflush(stdout);
-//}
-
-void init()
+void* InterpretScopeScript(int scope_id, ident_t script)
 {
-   // get EXE path
-   Path execPath;
-   getSelfPath(execPath);
-
-   _Instance = new ELENARTMachine(ROOT_PATH, execPath.c_str());
-
-   void* messageSection = nullptr;
-   void* mattributeSection = nullptr;
-   ELENARTMachine::ImageSection section;
-   section.init((void*)IMAGE_BASE, 0x1000000);
-
-   size_t ptr = 0;
-   MemoryReader reader(&section);
-
-   ELFHelper::seekRODataSegment(reader, ptr);
-
-   mattributeSection = (void*)ptr;
-
-   reader.seek(ptr - IMAGE_BASE);
-   size_t maSectionSize = reader.getDWord();
-
-   if(reader.seek(ptr - IMAGE_BASE + maSectionSize + 4)) {
-      messageSection = (void*)(IMAGE_BASE + reader.Position());
-
-      //printInfo((int)mattributeSection);
-      //printInfo((int)messageSection);
-      //printInfo((int)maSectionSize);
-
-      _Instance->init(messageSection, mattributeSection, CONFIG_PATH);
+   if (!session) {
+      newSession();
    }
-   //else printInfo(0);
+
+   return session->translate(scope_id, script);
 }
 
-void InitializeSTA(void* systemEnv, void* exceptionHandler, void* criticalHandler, void* entryPoint, ProgramHeader* header)
+void* InterpretFile(const char* pathStr, int encoding, bool autoDetect)
 {
-   header->root_exception_struct.core_catch_addr = (pos_t)exceptionHandler;
-
-   // initialize the critical exception handler
-   __routineProvider.InitCriticalStruct(&header->root_critical_struct, (pos_t)criticalHandler);
-
-   // initialize system env variable
-   _SystemEnv = systemEnv;
-
-   if (_Instance == nullptr)
-      init();
-
-   // start the system
-   _Instance->startSTA(header, (SystemEnv*)systemEnv, entryPoint);
-
-   ::exit(0);
-}
-
-void InitializeMTA(void* systemEnv, void* exceptionHandler, void* criticalHandler, void* entryPoint, ProgramHeader* header)
-{
-//   header->root_exception_struct.core_catch_addr = (pos_t)exceptionHandler;
-//
-//   // initialize the critical exception handler
-//   __routineProvider.InitCriticalStruct(&header->root_critical_struct, (pos_t)criticalHandler);
-//
-//   // initialize system env variable
-//   _SystemEnv = systemEnv;
-//
-//   if (_Instance == nullptr)
-//      init();
-//
-//   // start the system
-//   _Instance->startMTA(header, (SystemEnv*)systemEnv, entryPoint);
-}
-
-int StartThread(void* systemEnv, void* exceptionHandler, void* entryPoint, int index)
-{
-//   ProgramHeader header;
-//   // initialize the exception handler
-////   asm(
-////      "mov header.root_exception_struct.core_catch_frame, ebp\n\t"
-////      "mov header.root_exception_struct.core_catch_level, esp"
-////   );
-//
-//   header.root_exception_struct.core_catch_addr = (pos_t)exceptionHandler;
-//
-//   _Instance->startThread(&header, (SystemEnv*)systemEnv, entryPoint, index);
-
-   return 0;
-}
-
-void OpenFrame(void* systemEnv, void* frameHeader)
-{
-//   SystemRoutineProvider::OpenFrame((SystemEnv*)systemEnv, (FrameHeader*)frameHeader);
-}
-
-void CloseFrame(void* systemEnv, void* frameHeader)
-{
-//   SystemRoutineProvider::CloseFrame((SystemEnv*)systemEnv, (FrameHeader*)frameHeader);
-}
-
-void* GCCollect(void* roots, size_t size)
-{
-   return nullptr;
-}
-
-void Exit(int exitCode)
-{
-   _Instance->Exit(exitCode);
-}
-
-void StopThread(int exitCode)
-{
-//   _Instance->ExitThread((SystemEnv*)_SystemEnv, exitCode);
-}
-
-int ReadCallStack(void* instance, size_t framePosition, size_t currentAddress, size_t startLevel, int* buffer, size_t maxLength)
-{
-   return ((ELENARTMachine*)instance)->readCallStack(framePosition, currentAddress, startLevel, buffer, maxLength);
-}
-
-int LoadAddressInfo(size_t retPoint, char* lineInfo, int length)
-{
-   return _Instance->loadAddressInfo(retPoint, lineInfo, length);
-}
-
-int LoadClassName(void* object, char* buffer, int length)
-{
-   return _Instance->loadClassName((size_t)object, buffer, length);
-}
-
-void* EvaluateTape(void* tape)
-{
-   // !! terminator code
-   return NULL;
-}
-
-void* InterpretTape(void* tape)
-{
-   // !! terminator code
-   return NULL;
-}
-
-void* GetVMLastError(void* retVal)
-{
-   return NULL;
-}
-
-int LoadSubjectName(void* subject, char* lineInfo, int length)
-{
-   return _Instance->loadSubjectName((size_t)subject, lineInfo, length);
-}
-
-void* LoadSubject(void* subjectName)
-{
-   return _Instance->loadSubject((const char*)subjectName);
-}
-
-int LoadMessageName(void* message, char* lineInfo, int length)
-{
-   return _Instance->loadMessageName((ref_t)message, lineInfo, length);
-}
-
-void* LoadMessage(void* messageName)
-{
-   return _Instance->loadMessage((const char*)messageName);
-}
-
-void* LoadClassByString(void* systemEnv, void* referenceName)
-{
-   return _Instance->loadMetaAttribute((const char*)referenceName, caSerializable);
-}
-
-void* LoadClassByBuffer(void* systemEnv, void* referenceName, size_t index, size_t length)
-{
-   if (length < 0x100) {
-      IdentifierString str((const char*)referenceName, index, length);
-
-      return LoadClassByString(systemEnv, (void*)str.c_str());
+   if (!session) {
+      newSession();
    }
-   else {
-      DynamicString<char> str((const char*)referenceName, index, length);
 
-      return LoadClassByString(systemEnv, (void*)str.str());
-   }
+   return session->translate(0, pathStr, encoding, autoDetect);
 }
 
-void* LoadSymbolByString(void* systemEnv, void* referenceName)
+void* InterpretScopeFile(int scope_id, const char* pathStr, int encoding, bool autoDetect)
 {
-   return _Instance->loadMetaAttribute((const char*)referenceName, caSymbolSerializable);
+   if (!session) {
+      newSession();
+   }
+
+   return session->translate(scope_id, pathStr, encoding, autoDetect);
 }
 
-void* LoadSymbolByBuffer(void* systemEnv, void* referenceName, size_t index, size_t length)
+void Release(void* tape)
 {
-   if (length < 0x100) {
-      IdentifierString str((const char*)referenceName, index, length);
-
-      return LoadSymbolByString(systemEnv, (void*)str.c_str());
+   if (!session) {
+      newSession();
    }
-   else {
-      DynamicString<char> str((const char*)referenceName, index, length);
 
-      return LoadSymbolByString(systemEnv, (void*)str.str());
+   session->free(tape);
+}
+
+int GetStatus(char* buffer, int maxLength)
+{
+   if (session) {
+      ident_t error = session->getLastError();
+      size_t length = getlength(error);
+      if (buffer != NULL) {
+         if ((int)length > maxLength)
+            length = maxLength;
+
+         Convertor::copy(buffer, error, length, length);
+      }
+
+      return length;
    }
+   else return 0;
+}
+
+int NewScope()
+{
+   if (session) {
+      return session->newScope();
+   }
+   else return -1;
 }
--- elenasrc2/elenasm/vs/resource.h
+++ elenasrc2/elenasm/vs/resource.h
@@ -15,7 +15,7 @@
 #endif
 #endif
 
-#define ELENASM_REVISION_NUMBER        9
+#define ELENASM_REVISION_NUMBER        10
 
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
