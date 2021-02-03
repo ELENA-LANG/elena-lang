b9c78c2c11e0809d6687e37b09fef28efb8bde6a
Aleksey Rakov <arakov@yandex.ru>
2021-02-03 17:20:17 +0100

    #443 : creating core assembly for power8 64-bit mode

---
+++ asm/ppc207_64/core.asm
@@ -0,0 +1,20 @@
+// --- Predefined References  --
+define GC_ALLOC	            10001h
+define HOOK                 10010h
+define INVOKER              10011h
+define INIT_RND             10012h
+define CALC_SIZE            1001Fh
+define GET_COUNT            10020h
+define THREAD_WAIT          10021h
+define GC_ALLOCPERM	    10031h
+
+define CORE_GC_TABLE        20002h
+define CORE_STATICROOT      20005h
+define CORE_TLS_INDEX       20007h
+define THREAD_TABLE         20008h
+define CORE_MESSAGE_TABLE   2000Ah
+define CORE_ET_TABLE        2000Bh
+define SYSTEM_ENV           2000Ch
+define VOID           	    2000Dh
+define VOIDPTR              2000Eh
+
