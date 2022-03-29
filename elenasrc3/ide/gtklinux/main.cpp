2c7a1f422d11111c55f2f50aa7dafb3e86d85aa3
arakov <arakov@yandex.ru>
2022-03-29 11:35:21 +0200

    updating linux project

---
+++ elenasrc3/ide/gtklinux/main.cpp
@@ -0,0 +1,14 @@
+//---------------------------------------------------------------------------
+//		E L E N A   P r o j e c t:  ELENA IDE
+//      Linux-GTK+ program entry
+//                                             (C)2021/2022, by Aleksey Rakov
+//---------------------------------------------------------------------------
+
+#include "gtklinux\gtkcommon.h"
+
+int main(int args, char* argv[])
+{
+   Gtk::Main kit(argc, argv);
+
+   return 0;
+}
