b9b1521aa2695a01b325ae1ca4f8580552fdbe77
Aleksey Rakov <aleksey.rakov@appsfactory.de>
2022-08-07 19:53:55 +0200

    fixing elenart

--- asm/aarch64/core60.asm
+++ asm/aarch64/core60.asm
@@ -1719,6 +1719,7 @@ inline %0FEh
 
   add     x15, sp, #16
   ldr     x2, [x15]  
+  ldr     x3, [x15, #8]  
 
   movz    x16,  __ptr32lo_1
   movk    x16,  __ptr32hi_1, lsl #16
--- elenasrc3/elenart/vs/resource.h
+++ elenasrc3/elenart/vs/resource.h
@@ -15,7 +15,7 @@
 #endif
 #endif
 
-#define ELENART_REVISION_NUMBER        0x0008
+#define ELENART_REVISION_NUMBER        0x0009
 
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
