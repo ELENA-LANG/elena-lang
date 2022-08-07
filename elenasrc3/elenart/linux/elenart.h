eee75382c5b73d5d4b983c4e00a688fdf787b53e
Aleksey Rakov <aleksey.rakov@appsfactory.de>
2022-08-07 12:29:25 +0200

    fixing broken code

--- elenasrc3/elenart/linux/elenart.h
+++ elenasrc3/elenart/linux/elenart.h
@@ -19,7 +19,8 @@
 
 extern "C"
 {
-   DLL_PUBLIC void InitializeSTA(elena_lang::SystemEnv* env, elena_lang::SymbolList* entryList);
+   DLL_PUBLIC void InitializeSTA(elena_lang::SystemEnv* env, elena_lang::SymbolList* entryList, 
+      elena_lang::ExceptionStruct* ex_struct);
 }
 
 #endif // ELENART_H_INCLUDED
