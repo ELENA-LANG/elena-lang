603f14d0afa1f9c89b356955c4f9581316c9592e
Aleksey Rakov <aleksey.rakov@appsfactory.de>
2022-08-07 22:05:37 +0200

    fixing code

--- elenasrc3/engine/linux/lnxroutines.cpp
+++ elenasrc3/engine/linux/lnxroutines.cpp
@@ -126,19 +126,19 @@ static void ELENASignalHandler(int sig, siginfo_t* si, void* unused)
 
    switch (sig) {
       case SIGFPE:
-         u->uc_mcontext.mc_gregs.r9 = u->uc_mcontext.mc_gregs.pc;
-         u->uc_mcontext.mc_gregs.r0 = ELENA_ERR_DIVIDE_BY_ZERO;
-         u->uc_mcontext.mc_gregs.pc = CriticalHandler;
+         u->uc_mcontext.gregs[9] = u->uc_mcontext.gregs[33];
+         u->uc_mcontext.gregs[0] = ELENA_ERR_DIVIDE_BY_ZERO;
+         u->uc_mcontext.gregs[33] = CriticalHandler;
          break;
       case SIGSEGV:
-         u->uc_mcontext.mc_gregs.r9 = u->uc_mcontext.mc_gregs.pc;
-         u->uc_mcontext.mc_gregs.r0 = ELENA_ERR_ACCESS_VIOLATION;
-         u->uc_mcontext.mc_gregs.pc = CriticalHandler;
+         u->uc_mcontext.gregs[9] = u->uc_mcontext.gregs[33];
+         u->uc_mcontext.gregs[0] = ELENA_ERR_ACCESS_VIOLATION;
+         u->uc_mcontext.gregs[33] = CriticalHandler;
          break;
       default:
-         u->uc_mcontext.mc_gregs.r9 = u->uc_mcontext.mc_gregs.pc;
-         u->uc_mcontext.mc_gregs.r0 = ELENA_ERR_CRITICAL;
-         u->uc_mcontext.mc_gregs.pc = CriticalHandler;
+         u->uc_mcontext.gregs[9] = u->uc_mcontext.gregs[33];
+         u->uc_mcontext.gregs[0] = ELENA_ERR_CRITICAL;
+         u->uc_mcontext.gregs[33] = CriticalHandler;
          break;
    }
 }
