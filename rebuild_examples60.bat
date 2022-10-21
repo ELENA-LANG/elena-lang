b4b99dbe12170145885765b3837f922a28ce2876
Aleksey Rakov <aleksey.rakov@appsfactory.de>
2022-10-21 16:58:32 +0200

    fixing example script

--- rebuild_examples60.bat
+++ rebuild_examples60.bat
@@ -1,4 +1,9 @@
-bin\elc examples60\console\helloworld\helloworld.prj
+bin\elena-cli examples60\console\helloworld\helloworld.prj
+@echo off 
+if %ERRORLEVEL% EQU -2 GOTO CompilerError
+@echo on
+
+bin\elena64-cli examples60\console\helloworld\helloworld.prj
 @echo off 
 if %ERRORLEVEL% EQU -2 GOTO CompilerError
 @echo on
