7a076995a09a60b7b23d171bccc3bf719b579932
Aleksey Rakov <aleksey.rakov@appsfactory.de>
2022-10-22 17:45:29 +0200

    fixing ppc64le invoker

--- asm/ppc64le/core60_lnx.asm
+++ asm/ppc64le/core60_lnx.asm
@@ -33,12 +33,12 @@ procedure % INVOKER
 
   mflr  r0
 
-  std   r0,  192(r1)
   std   r31, 168(r1)
   std   r30, 160(r1)
   std   r29, 152(r1)
   std   r28, 144(r1)
   std   r27, 136(r1)
+  std   r0,  128(r1)
 
   li    r16, 0
   std   r16, -10h(r1)     // ; save frame pointer
@@ -57,7 +57,7 @@ procedure % INVOKER
   ld   r28, 144(r1)
   ld   r27, 136(r1)
 
-  ld   r0,  192(r1)
+  ld   r0,  128(r1)
   mtlr r0
 
   addi  r1, r1, 176
