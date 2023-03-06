e59f9c9c48ef20cd86ca5fc1b398dc2c0f351b11
Aleksey Rakov <aleksey.rakov@appsfactory.de>
2023-03-06 17:48:11 +0100

    [+]adding new project : elenasm

---
+++ elenasrc3/elenasm/smcommon.h
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+//		E L E N A   P r o j e c t:  ELENA command-Line Compiler
+//
+//		This file contains the compiler common interfaces & types
+//
+//                                             (C)2021-2023, by Aleksey Rakov
+//---------------------------------------------------------------------------
+
+#ifndef RTCOMMON_H
+#define RTCOMMON_H
+
+namespace elena_lang
+{
+
+#define ELENASM_REVISION_NUMBER        0x0001
+
+}
+
+#endif
