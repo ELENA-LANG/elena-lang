bc08e12a762ea9c78c57080328a0c9f1387b0b06
Aleksey Rakov <aleksey.rakov@appsfactory.de>
2023-05-30 14:51:20 +0200

    tracing

--- elenasrc3/elenart/elenartmachine.cpp
+++ elenasrc3/elenart/elenartmachine.cpp
@@ -200,7 +200,7 @@ void ELENARTMachine :: startSTA(SystemEnv* env, void* entry)
    // executing the program
    int retVal = execute(env, entry);
 
-   printf("exiting");
+   //printf("exiting");
 
    unsigned n = 0x800000;
    unsigned m = 0x800000;
