8c55b175b10499bfc371dcaf5b8d2469a3e17a0a
Aleksey Rakov <aleksey.rakov@appsfactory.de>
2023-08-28 22:47:31 +0200

    fixing startup code

--- asm/ppc64le/core60.asm
+++ asm/ppc64le/core60.asm
@@ -2119,12 +2119,13 @@ end
 // ; system startup
 inline %4CFh
 
+  lis     r2, rdata32_hi : %CORE_TOC
+  addi    r2, r2, rdata32_lo : %CORE_TOC
+
+  ld      r16, toc_data(r2)
   addis   r16, r16, data_disp32hi : %CORE_SINGLE_CONTENT
   addi    r16, r16, data_disp32lo : %CORE_SINGLE_CONTENT
   std     r1, tt_stack_root(r16)
-
-  lis     r2, rdata32_hi : %CORE_TOC
-  addi    r2, r2, rdata32_lo : %CORE_TOC
 
   ld      r12, toc_prepare(r2)
   mtctr   r12            
