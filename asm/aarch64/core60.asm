86babb3c271b44d1b395c3ce399da010aa3d4e7f
arakov <arakov@yandex.ru>
2025-03-12 14:46:36 +0100

    [ADDED] aarch64 : fln

--- asm/aarch64/core60.asm
+++ asm/aarch64/core60.asm
@@ -81,6 +81,23 @@ structure % CORE_MATH_TABLE
   dbl "2.33184211722314911771e2"   // ; 48
   dbl "4.36821166879210612817e3"   // ; 56
 
+  dbl "1.41421356237309504880"     // ; 64
+
+  dbl "1.01875663804580931796e-4"  // ; 72
+  dbl "4.97494994976747001425e-1"  // ; 80
+  dbl "4.70579119878881725854e0"   // ; 88
+  dbl "1.44989225341610930846e1"   // ; 96
+  dbl "1.79368678507819816313e1"   // ; 104
+  dbl "7.70838733755885391666e0"   // ; 112
+
+  dbl "1.12873587189167450590e1"   // ; 120
+  dbl "4.52279145837532221105e1"   // ; 128
+  dbl "8.29875266912776603211e1"   // ; 136
+  dbl "7.11544750618563894466e1"   // ; 144
+  dbl "2.31251620126765340583e1"   // ; 152
+
+  dbl "6.9314718055994530942e-1"   // ; 160
+
 end
  
 structure % CORE_SINGLE_CONTENT
@@ -1111,6 +1128,147 @@ end
 
 // ; fln
 inline %07Bh
+
+  movz    x17, rdata_ptr32lo : %CORE_MATH_TABLE
+  movk    x17, rdata_ptr32hi : %CORE_MATH_TABLE, lsl #16
+
+  add     x19, x29, __arg12_1 // ; dest (x19)
+
+  ldr     d17, [x0]           // ; x (d17)
+
+//;   udi_t val;
+//;   double z (d21), px(d18), qx(d19);
+//;   int32_t ipart (x18), fpart (x16);
+
+//;   val.f = x;
+  str     d17, [x19]
+
+  //; extract exponent and part of the mantissa */
+
+//;   fpart = val.s.i1 & FM_DOUBLE_MMASK;
+  movz    x20, #0FFFFh
+  movk    x20, #0Fh, lsl #16
+
+  ldrsw   x16, [x19, #4]
+  and     x16, x16, x20
+//;   ipart = val.s.i1 & FM_DOUBLE_EMASK;
+  movz    x20, #7FF0h
+  ldrsw   x18, [x19, #4]
+  and     x18, x18, x20
+
+//;   /* set exponent to 0 to get the prefactor to 2**ipart */
+//;   fpart |= FM_DOUBLE_EZERO;
+  movz    x20, #3FF0h
+  orr     x16, x16, x20
+//;   val.s.i1 = fpart;
+  str     w16, [x19, #4]
+//;   x = val.f;
+  str     d17, [x19]
+
+//;   /* extract exponent */
+//;   ipart >>= FM_DOUBLE_MBITS;
+  movz    x20, #20
+  lsr     x18, x18, x20
+
+//;   ipart -= FM_DOUBLE_BIAS;
+  movz    x20, #1023
+  sub     x18, x18, x20
+
+//;   /* the polynomial is computed for sqrt(0.5) < x < sqrt(2),
+//;      but we have the mantissa in the interval 1 < x < 2.
+//;      adjust by dividing x by 2 and incrementing ipart, if needed. */
+//;   if (x > FM_DOUBLE_SQRT2) {
+  ldr     d20, [x16, #64]
+  fcmp    d17, d20
+  blt     labSkip
+  beq     labSkip
+
+//;      x *= 0.5;
+  ldr     d20, [x17, #40]
+  fmul    d17, d17, d20
+//;      ++ipart;
+  add     x18, x18, #1
+//;   }
+labSkip:
+
+//;   /* use polynomial approximation for log(1+x) */
+//;   x -= 1.0;
+  ldr     d20, [x17, #32]
+  fsub    d17, d17, d20
+
+//;   px = fm_log2_p[0];
+  ldr     d18, [x17, #72]
+
+//;   px = px * x + fm_log2_p[1];
+  ldr     d20, [x17, #80]
+  fmul    d18, d18, d17
+  fadd    d18, d18, d20
+
+//;   px = px * x + fm_log2_p[2];
+  ldr     d20, [x17, #88]
+  fmul    d18, d18, d17
+  fadd    d18, d18, d20
+
+//;   px = px * x + fm_log2_p[3];
+  ldr     d20, [x17, #96]
+  fmul    d18, d18, d17
+  fadd    d18, d18, d20
+
+//;   px = px * x + fm_log2_p[4];
+  ldr     d20, [x17, #104]
+  fmul    d18, d18, d17
+  fadd    d18, d18, d20
+
+//;   px = px * x + fm_log2_p[5];
+  ldr     d20, [x17, #112]
+  fmul    d18, d18, d17
+  fadd    d18, d18, d20
+
+//;   qx = x + fm_log2_q[0];
+  ldr     d20, [x17, #120]
+  fadd    d19, d17, d20
+
+//;   qx = qx * x + fm_log2_q[1];
+  ldr     d20, [x17, #128]
+  fmul    d19, d19, d17
+  fadd    d19, d19, d20
+
+//;   qx = qx * x + fm_log2_q[2];
+  ldr     d20, [x17, #136]
+  fmul    d19, d19, d17
+  fadd    d19, d19, d20
+
+//;   qx = qx * x + fm_log2_q[3];
+  ldr     d20, [x17, #144]
+  fmul    d19, d19, d17
+  fadd    d19, d19, d20
+
+//;   qx = qx * x + fm_log2_q[4];
+  ldr     d20, [x17, #152]
+  fmul    d19, d19, d17
+  fadd    d19, d19, d20
+
+//;   z = x * x;
+  fmul    d21, d17, d17
+
+//;   z = x * (z * px / qx) - 0.5 * z + x;
+  fmul    d20, d21, d18
+  fdiv    d20, d20, d19
+  fmul    d22, d20, d17
+
+  ldr     d20, [x17, #40]
+  fmul    d20, d20, d21
+  fsub    d22, d22, d20 
+  fadd    d21, d22, d17
+
+//;   z += ((double)ipart) * FM_DOUBLE_LOGEOF2;
+  scvtf   d20, x18
+  ldr     d18, [x17, #160]
+  fmul    d20, d20, d18
+  fadd    d21, d21, d20
+
+  str     d21, [x19]
+
 end
 
 // ; fsin
