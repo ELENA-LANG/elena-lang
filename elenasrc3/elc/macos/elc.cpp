d380b7ab6f880b3d442f310c6637b0432d776048
arakov <arakov@yandex.ru>
2025-03-12 22:53:50 +0100

    [ADDED] #708 : introducing macos subversion

---
+++ elenasrc3/elc/macos/elc.cpp
@@ -0,0 +1,15 @@
+//---------------------------------------------------------------------------
+//		E L E N A   P r o j e c t:  ELENA command-Line Compiler
+//
+//		This file contains the main body of the macOS command-line compiler
+//
+//                                             (C)2025, by Aleksey Rakov
+//---------------------------------------------------------------------------
+
+#include "elena.h"
+// --------------------------------------------------------------------------
+#include "cliconst.h"
+
+int main(int argc, char* argv[])
+{
+}
