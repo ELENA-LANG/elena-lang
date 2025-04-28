56142dd4ca9dcbf45f0880db10d565f5356d4007
arakov <arakov@yandex.ru>
2025-04-28 09:45:58 +0200

    tracing

--- elenasrc3/elc/linux/pathmanager.cpp
+++ elenasrc3/elc/linux/pathmanager.cpp
@@ -11,6 +11,12 @@
 #include "pathmanager.h"
 #include <unistd.h>
 
+#if defined(__FreeBSD__) 
+
+#include <sys/sysctl.h>
+
+#endif
+
 using namespace elena_lang;
 
 // --- PathHelper ---
@@ -24,7 +30,7 @@ inline bool loadAppPath(char* appPath, size_t len)
    int mib[4];
    mib[0] = CTL_KERN;
    mib[1] = KERN_PROC;
-   mib[2] = KERN_PROC_PATHNAME;
+   mib[2]1 = KERN_PROC_PATHNAME;
    mib[3] = -1;
    sysctl(mib, 4, appPath, len, nullptr, 0);
 
