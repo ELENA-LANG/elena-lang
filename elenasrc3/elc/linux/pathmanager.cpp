cf536299a6eeafed846cc512a59d3871b41bb0a4
arakov <arakov@yandex.ru>
2025-04-28 09:50:29 +0200

    tracing

--- elenasrc3/elc/linux/pathmanager.cpp
+++ elenasrc3/elc/linux/pathmanager.cpp
@@ -30,7 +30,7 @@ inline bool loadAppPath(char* appPath, size_t len)
    int mib[4];
    mib[0] = CTL_KERN;
    mib[1] = KERN_PROC;
-   mib[2]1 = KERN_PROC_PATHNAME;
+   mib[2] = KERN_PROC_PATHNAME;
    mib[3] = -1;
    sysctl(mib, 4, appPath, &len, nullptr, 0);
 
