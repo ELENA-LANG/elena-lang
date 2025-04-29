3fc84748301a8ca38b669b0337ba557210d8757a
arakov <arakov@yandex.ru>
2025-04-29 17:40:05 +0200

    tracing

--- elenasrc3/elc/linux/elfimage.cpp
+++ elenasrc3/elc/linux/elfimage.cpp
@@ -544,7 +544,7 @@ void Elf64ImageFormatter :: fillElfData(ImageProviderBase& provider, ElfData& el
 
 #if defined(__FreeBSD__)
    dynamicWriter.writeQWord(DT_RELA);
-   dynamicWriter.writeQReference(importRef, relGlobalOffset);
+   dynamicWriter.writeQReference(importRef, /*relGlobalOffset*/0);
 
    dynamicWriter.writeQWord(DT_RELASZ);
    dynamicWriter.writeQWord(/*global_count * 24*/0);
