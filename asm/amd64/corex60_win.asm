713761b82ab414d67696dd35de59bcab8558c1c5
arakov <arakov@yandex.ru>
2025-06-10 10:49:53 +0200

    housekeeping : refactoring

--- asm/amd64/corex60_win.asm
+++ asm/amd64/corex60_win.asm
@@ -4,15 +4,3 @@ define VEH_HANDLER       	10003h
 define CORE_TLS_INDEX       	20004h
 
 // ; ==== System commands ===
-
-// VEH_HANDLER() 
-procedure % VEH_HANDLER
-
-  mov  esi, edx
-  mov  edx, eax   // ; set exception code
-
-  mov  rcx, gs:[58h]
-  mov  rcx, [rcx]
-  jmp  [rcx]
-
-end
