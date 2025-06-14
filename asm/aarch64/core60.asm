e1e6937900ae6f5a94d53575cce2cba95a56ea42
arakov <arakov@yandex.ru>
2025-06-14 22:05:58 +0200

    [ADDED] aarch64 : fcosdp opcode

--- asm/aarch64/core60.asm
+++ asm/aarch64/core60.asm
@@ -1421,6 +1421,122 @@ end
 
 // ; fcos
 inline %07Dh
+
+labStart:
+  ldr     d0, [x0]           // ; x (d0)
+
+  movz    x20, rdata_ptr32lo : %CORE_MATH_TABLE
+  movk    x20, rdata_ptr32hi : %CORE_MATH_TABLE, lsl #16
+
+  ldr     d1, [x20, #184] //; d1 <- PI_2
+  fsub    d0, d1, d0
+
+  add     x19, x29, __arg12_1 // ; dest (x19)
+
+  ldr     d1, [x20, #176] //; d1 <- PI_4
+  adr     x23, sin2a0     //; origin of jump table
+
+  fdiv    d2, d0, d1     //; d2 <- x / PI_4
+  frintm  d2, d2         //; d2 <- floor (d2)
+  fmsub   d0, d1, d2, d0 //; d0 <- x mod PI_4
+
+  fcvtas  x21, d2
+  and     x21, x21, #7
+  add     x23, x23, x21, lsl #4
+  br      x23
+
+sin2a0: 
+  b       sin1a
+  nop
+  nop
+  nop
+sin2a1:
+  fsub    d0, d1, d0
+  b       cos1a
+  nop
+  nop
+sin2a2: 
+  b       cos1a
+  nop
+  nop
+  nop
+sin2a3:
+  fsub    d0, d1, d0
+  b       sin1a
+  nop
+  nop
+sin2a4:
+  fneg    d0, d0
+  b       sin1a
+  nop
+  nop
+sin2a5:
+  fsub    d0, d1, d0
+  b       cos1a_neg
+  nop
+  nop
+sin2a6:
+  b       cos1a_neg
+  nop
+  nop
+  nop
+sin2a7:
+  fsub    d0, d0, d1
+
+sin1a:
+  movz    x20, rdata_ptr32lo : %CORE_MATH_TABLE             // ; sin1a1
+  movk    x20, rdata_ptr32hi : %CORE_MATH_TABLE, lsl #16
+  add     x20, x20, #192
+
+  ldp     d4, d5, [x20], #16
+  ldp     d6, d7, [x20]
+
+  fmul    d1,d0,d0 // d1 <- x^2
+  fmul    d2,d1,d0 // d2 <- x^3
+  fmul    d3,d2,d1 // d3 <- x^5
+  fmul    d1,d1,d3 // d1 <- x^7
+  fmadd   d0,d5,d2,d0
+  fmadd   d0,d6,d3,d0
+  fmadd   d0,d7,d1,d0
+  b       labEnd
+
+cos1a_neg:
+  movz    x20, rdata_ptr32lo : %CORE_MATH_TABLE             // ; cos1a1
+  movk    x20, rdata_ptr32hi : %CORE_MATH_TABLE, lsl #16
+  add     x20, x20, #224
+
+  ldp     d4, d5, [x20], #16
+  ldp     d6, d7, [x20]
+
+  fmul    d1,d0,d0 // d1 <- x^2
+  fmul    d2,d1,d1 // d2 <- x^4
+  fmul    d3,d2,d1 // d3 <- x^6
+  fmov    d0,d4 // d0 <- 1
+  fmadd   d0,d1,d5,d0
+  fmadd   d0,d2,d6,d0
+  fmadd   d0,d3,d7,d0
+  fneg    d0, d0
+  b       labEnd
+
+cos1a:
+  movz    x20, rdata_ptr32lo : %CORE_MATH_TABLE             // ; cos1a1
+  movk    x20, rdata_ptr32hi : %CORE_MATH_TABLE, lsl #16
+  add     x20, x20, #224
+
+  ldp     d4,d5,[x20],16
+  ldp     d6,d7,[x20]
+
+  fmul    d1,d0,d0 // d1 <- x^2
+  fmul    d2,d1,d1 // d2 <- x^4
+  fmul    d3,d2,d1 // d3 <- x^6
+  fmov    d0,d4 // d0 <- 1
+  fmadd   d0,d1,d5,d0
+  fmadd   d0,d2,d6,d0
+  fmadd   d0,d3,d7,d0
+
+labEnd:
+  str     d0, [x19]
+
 end
 
 // ; farchtan
