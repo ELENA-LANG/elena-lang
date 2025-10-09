03674caa63110cc6a89c2fee2dca2fb5d8143bff
arakov <arakov@yandex.ru>
2025-10-09 10:12:03 +0200

    [ADDED] shorthand syntax for lambda function without arguments : ([] => "Hello from Func")

--- elenasrc3/elc/cliconst.h
+++ elenasrc3/elc/cliconst.h
@@ -13,7 +13,7 @@
 
 namespace elena_lang
 {
-   #define ELC_REVISION_NUMBER               0x0121
+   #define ELC_REVISION_NUMBER               0x0122
 
    constexpr auto WARNING_RET_CODE           =  2;
 
