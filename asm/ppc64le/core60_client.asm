1027a7d470991c1836c90aacb3db5790c33ef28f
arakov <arakov@yandex.ru>
2025-11-03 20:54:35 +0100

    tracing

--- asm/ppc64le/core60_client.asm
+++ asm/ppc64le/core60_client.asm
@@ -4,6 +4,18 @@ define VEH_HANDLER       10003h
 define CORE_TOC          20001h
 define CORE_ET_TABLE     2000Bh
 
+// ; TOC TABLE OFFSETS
+define toc_import            0000h
+define toc_rdata             0008h
+define toc_mdata             0010h
+define toc_code              0018h
+define toc_gctable           0020h
+define toc_alloc             0028h
+define toc_data              0030h
+define toc_stat              0038h
+define toc_allocperm         0040h
+define toc_prepare           0048h
+
 // ; system startup
 inline %4CFh
 
