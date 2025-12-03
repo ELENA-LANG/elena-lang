95b513a8a63519b8591a077c11874a8e78f76a0e
arakov <arakov@yandex.ru>
2025-12-03 10:15:56 +0100

    fixing unit tests

--- elenasrc3/elc/cliconst.h
+++ elenasrc3/elc/cliconst.h
@@ -13,7 +13,7 @@
 
 namespace elena_lang
 {
-   #define ELC_REVISION_NUMBER               0x0129
+   #define ELC_REVISION_NUMBER               0x012A
 
    constexpr auto WARNING_RET_CODE           =  2;
 
